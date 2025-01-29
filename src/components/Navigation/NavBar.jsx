325058839ef9ba516038d911b9dfdeb0a50b9e01
Al Prince Llavan <alprincellavan2019@gmail.com>
2025-01-29 21:33:20 +0800

    home page

--- src/components/Navigation/NavBar.jsx
+++ src/components/Navigation/NavBar.jsx
@@ -4,6 +4,8 @@ import { FaSquareInstagram } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 import { CiUser } from "react-icons/ci";
 import { BsTelephone } from "react-icons/bs";
+import { MdOutlineLocationOn } from "react-icons/md";
+
 
 
 
@@ -14,40 +16,46 @@ export default function NavBar() {
     const navigate = useNavigate();
 
     return (
-        <div className="nav-bar">
-            <div className="">
-                <div className="contact flex justify-between items-center py-2 px-16 bg-[#001A6E] text-white">
-                    <div className="social flex space-x-4">
-                        <FaFacebook className="text-xl text-white"/>
-                        <FaSquareInstagram className="text-xl text-white"/>
-                        <FaLinkedin className="text-xl text-white"/>
+        <div className="nav-bar text-[#181C14]">
+            <div className="text-[#181C14]">
+                <div className="contact flex justify-between items-center py-2 px-16 bg-[#E2E0C8] text-[#181C14]">
+                    <div className="social flex space-x-4 text-[#181C14]">
+                        <FaFacebook className="text-xl"/>
+                        <FaSquareInstagram className="text-xl"/>
+                        <FaLinkedin className="text-xl"/>
                     </div>
-                    <p className="font-medium text-white font-sans ">PACH | DRUGMART AND MEDICAL CLINIC</p>
-                    <div className="font-medium text-white flex items-center"><BsTelephone/>CALL NOW: 0000 000 0000</div>
+                    <p className="font-medium font-sans text-[#181C14]">PACH | DRUGMART AND MEDICAL CLINIC</p>
+                    <div className="font-medium flex items-center"><BsTelephone/>CALL NOW: 0000 000 0000</div>
                 </div>
-                <div className="nav flex justify-between items-center border-b-1 border-gray-300 px-16 bg-[#155C9C]">
-                    <img src="./images/Wordmark_BW.png" alt="" className="w-[12rem] cursor-pointer" onClick={()=> navigate("/home")}/>
-                    <input type="text" placeholder="Search for products" className="border-2 flex-grow mx-20 rounded h-[2.5rem] px-4 bg-gray-200 border-gray-300 border-opacity-10 rounded-2xl"/>
-                    <div className="text-white flex items-center space-x-4 cursor-pointer hover:text-[#5CB338] transition-colors duration-300 ease-in-out" onClick={
+                <div className="nav flex justify-between items-center border-b-1 border-gray-300 px-16 bg-[#FFFFFF]">
+                    <div className="flex items-center w-[50%] space-x-4">
+                        <img src="/images/Wordmark.2.png" alt="Pach" className="w-[5rem]"/>
+                        <input type="text" placeholder="Search for products" className="border-1 h-[3rem] w-full px-4 py-2 rounded-3xl"/>
+                    </div>
+                    <div className="flex items-center space-x-4 cursor-pointer text-[#181C14] hover:text-[#5CB338] transition-colors duration-300 ease-in-out" onClick={
                         () => navigate("/login")
                     }>
-                        <CiUser className="text-4xl text-white"/>
+                        <div className="flex items-center mr-12">
+                            <MdOutlineLocationOn className="text-2xl"/>
+                            <h1 className=" font-medium t"> Yellow Bell, Colon, City of Naga, Cebu</h1>
+                        </div>
+                        <CiUser className="text-4xl"/>
                         <div>
-                            <h2 className="font-bold text-white">Account</h2>
-                            <p className="text-xs text-white">Login / Register</p>
+                            <h2 className="font-bold">Account</h2>
+                            <p className="text-xs">Login / Register</p>
                         </div>
                     </div>
                 </div>
-                <div className="px-16 flex py-4 space-x-8 border-b-3 border-[#155C9C] font-medium text-[#213555] bg-[#155C9C]">
-                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out text-white">SHOP</p>
-                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out text-white">HEALTH & ADVICE</p>
-                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out text-white"
+                <div className="px-16 flex py-4 space-x-8 font-semibold border-[#155C9C] text-[#181C14] bg-[#FFFFFF] text-sm">
+                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out">SHOP</p>
+                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out">HEALTH & ADVICE</p>
+                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out"
                         onClick={()=>navigate("/about")}
                     >ABOUT</p>
-                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out text-white"
+                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out"
                         onClick={()=>navigate("/booking")}
                     >BOOK</p>
-                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out text-white"
+                    <p className="cursor-pointer hover:text-[#32DBBE] transition-colors duration-300 ease-in-out"
                         onClick={()=>navigate("/contact")}
                     >CONTACT</p>
                     
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,109 +7,119 @@ export default function Home() {
     const navigate = useNavigate();
 
     return (
-        <div className="home px-16">
-            <div className="tagline flex items-center font-mono text-3xl bg-[url('/images/fortagline.jpg')] bg-cover bg-center h-[35rem] mt-8 shadow-[0px_4px_6px_rgba(0,0,0,0.1),0px_1px_10px_rgba(0,0,0,0.1)] relative rounded-xl">
-                <div className="z-0 absolute inset-0 bg-gradient-to-r from-[#155C9C] via-transparent to-transparent opacity-80 rounded-xl"></div>
-                <div>
-                    <h1 className="text-white z-10 relative ml-4">"Your source</h1>
-                    <h1 className="text-white z-10 relative ml-16">of cost </h1>
-                    <h1 className="text-white z-10 relative ml-8">effective healthcare"</h1>
-                </div>
+        <div className="home">
+            <div className="tagline w-full h-[7rem] bg-gray-200 flex items-center justify-center">
+                <h1 className="tagline italic text-5xl text-[#155C9C]">“Your source of cost effective healthcare”</h1>
             </div>
-
-            <div className="flex items-center justify-center relative w-full mt-8">
-                <hr className="flex-grow border-1 border-gray-300"/>
-                <span className="absolute px-4 bg-white text-2xl text-[#213555] font-medium">Services</span>
-                <hr className="flex-grow border-1 border-gray-300" />
-            </div>
-            <div className="services mt-8 grid grid-cols-4 gap-8 justify-center h-screen">
-                <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
-                    <img className="w-full" src="./images/Blood Pressure.jpg" alt="Blood Pressure"/>
-                    <div className="px-6 py-4">
-                        <div className="text-xl mt-4 font-medium text-[#213555]">Blood Pressure</div>
-                    </div>
-                </div>
-
-                <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
-                    <img className="w-full" src="./images/CBS Test (Blood Sugar Test).jpg" alt="CBS Test"/>
-                    <div className="px-6 py-4">
-                        <div className="text-xl mt-4 font-medium text-[#213555]">CBS Test</div>
-                    </div>
-                </div>
-
-                <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
-                    <img className="w-full" src="./images/Electrocardiography.jpg" alt="Electrocardiography"/>
-                    <div className="px-6 py-4">
-                        <div className="text-xl mt-4 font-medium text-[#213555]">Electrocardiography</div>
-                    </div>
-                </div>
-
-                <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
-                    <img className="w-full" src="./images/On-site Consult.jpg" alt="On-site Consult"/>
-                    <div className="px-6 py-4">
-                        <div className="text-xl mt-4 font-medium text-[#213555]">On-site Consult</div>
-                    </div>
-                </div>
-
-                <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
-                    <img className="w-full" src="./images/Online Consul.jpg" alt="Online Consult"/>
-                    <div className="px-6 py-4">
-                        <div className="text-xl mt-4 font-medium text-[#213555]">Online Consult</div>
-                    </div>
-                </div>
-
-                <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
-                    <img className="w-full" src="./images/Online Pharmacy.jpg" alt="Online Pharmacy"/>
-                    <div className="px-6 py-4">
-                        <div className="text-xl mt-4 font-medium text-[#213555]">Online Pharmacy</div>
-                    </div>
-                </div>
-
-                <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
-                    <img className="w-full" src="./images/Shipping & Delivery.jpg" alt="Shipping & Delivery"/>
-                    <div className="px-6 py-4">
-                        <div className="text-xl mt-4 font-medium text-[#213555]">Shipping & Delivery</div>
-                    </div>
-                </div>
-
-                <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
-                    <img className="w-full" src="./images/Vaccination Program.jpg" alt="Vaccination Program"/>
-                    <div className="px-6 py-4">
-                        <div className="text-xl mt-4 font-medium text-[#213555]">Vaccination Program</div>
-                    </div>
-                </div>
-            </div>
-            <div className="bg-[#BFDBE4] h-screen mt-8 p-4 flex w-full">
-                <div className="w-[50%] p-4 flex flex-col">
-                    <h1 className="text-5xl font-bold text-[#155C9C]">Support and Inquiries</h1>
-                    <p className="mt-8 text-lg text-gray-700">Have questions about our products, prescriptions, or orders? We're here to help! Fill out the form below, and our team will get back to you as soon as possible. Whether you need assistance with a purchase, have a medical inquiry, or just want to share feedback, we’re happy to assist. Your health and convenience are our priority!</p>
-                    <p className="flex items-center underline mt-8"><AiOutlineMail className="text-xl mr-2"/> pachdrugmart1@gmail.com</p>
-                    <p className="flex items-center mt-4 underline"><BsTelephone className="mr-2"/>PACH Climaco - 09171403349</p>
-                    <p className="flex items-center mt-4 underline"><BsTelephone className="mr-2"/>PACH Kaya - 09171862245
-                    </p>
-                </div>
-                <div className="w-[50%] p-4">
-                    <form className="w-full h-full flex flex-col justify-between bg-white p-8 rounded-lg shadow-lg">
-                        <div>
-                            <div className="flex justify-between w-full mb-4">
-                                <input type="text" placeholder="*First Name" className="border border-gray-300 rounded px-4 py-2 w-[48%] focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
-                                <input type="text" placeholder="*Last Name" className="border border-gray-300 rounded px-4 py-2 w-[48%] focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
-                            </div>
-                            <div className="mb-4">
-                                <input type="email" placeholder="*Email" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
-                            </div>
-                            <div className="mb-4">
-                                <input type="tel" placeholder="*Phone Number" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
-                            </div>
-                            <div className="mb-4">
-                                <input type="text" placeholder="*Subject" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
-                            </div>
-                            <div className="mb-4">
-                                <textarea placeholder="*Message" className="border border-gray-300 rounded w-full h-[12rem] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#155C9C]"></textarea>
-                            </div>
+            <div className="bg-[#155C9C]  px-16 pt-8">
+                <div className="flex items-center justify-between">
+                    <div className="w-[35rem] rounded overflow-hidden shadow-2xl h-fit">
+                        <img className="w-full" src="./images/Online Consul.jpg" alt="Blood Pressure"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">Blood Pressure</div>
                         </div>
-                        <button className="py-2 bg-[#155C9C] w-full text-white font-medium rounded hover:bg-[#0D3A5C] transition duration-300">Submit</button>
-                    </form>
+                    </div>
+                    <div className="w-[35rem] rounded overflow-hidden shadow-2xl h-fit">
+                        <img className="w-full" src="./images/Blood Pressure.jpg" alt="Blood Pressure"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">Blood Pressure</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-center relative w-full mt-8">
+                    <hr className="flex-grow border-1 border-gray-300"/>
+                    <span className="absolute px-4 bg-white text-2xl text-[#213555] font-medium">Services</span>
+                    <hr className="flex-grow border-1 border-gray-300" />
+                </div>
+                <div className="services mt-8 grid grid-cols-4 gap-8 justify-center h-screen">
+                    <div className=" max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
+                        <img className="w-full" src="./images/Blood Pressure.jpg" alt="Blood Pressure"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">Blood Pressure</div>
+                        </div>
+                    </div>
+
+                    <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
+                        <img className="w-full" src="./images/CBS Test (Blood Sugar Test).jpg" alt="CBS Test"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">CBS Test</div>
+                        </div>
+                    </div>
+
+                    <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
+                        <img className="w-full" src="./images/Electrocardiography.jpg" alt="Electrocardiography"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">Electrocardiography</div>
+                        </div>
+                    </div>
+
+                    <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
+                        <img className="w-full" src="./images/On-site Consult.jpg" alt="On-site Consult"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">On-site Consult</div>
+                        </div>
+                    </div>
+
+                    <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
+                        <img className="w-full" src="./images/Online Consul.jpg" alt="Online Consult"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">Online Consult</div>
+                        </div>
+                    </div>
+
+                    <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
+                        <img className="w-full" src="./images/Online Pharmacy.jpg" alt="Online Pharmacy"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">Online Pharmacy</div>
+                        </div>
+                    </div>
+
+                    <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
+                        <img className="w-full" src="./images/Shipping & Delivery.jpg" alt="Shipping & Delivery"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">Shipping & Delivery</div>
+                        </div>
+                    </div>
+
+                    <div className="max-w-sm rounded overflow-hidden shadow-2xl h-fit transform transition duration-300 hover:scale-105">
+                        <img className="w-full" src="./images/Vaccination Program.jpg" alt="Vaccination Program"/>
+                        <div className="px-6 py-4 bg-[#FFFFFF]">
+                            <div className="text-xl mt-4 font-medium text-[#213555]">Vaccination Program</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="bg-[#BFDBE4] h-screen mt-8 p-4 flex w-full">
+                    <div className="w-[50%] p-4 flex flex-col">
+                        <h1 className="text-5xl font-bold text-[#155C9C]">Support and Inquiries</h1>
+                        <p className="mt-8 text-lg text-gray-700">Have questions about our products, prescriptions, or orders? We're here to help! Fill out the form below, and our team will get back to you as soon as possible. Whether you need assistance with a purchase, have a medical inquiry, or just want to share feedback, we’re happy to assist. Your health and convenience are our priority!</p>
+                        <p className="flex items-center underline mt-8"><AiOutlineMail className="text-xl mr-2"/> pachdrugmart1@gmail.com</p>
+                        <p className="flex items-center mt-4 underline"><BsTelephone className="mr-2"/>PACH Climaco - 09171403349</p>
+                        <p className="flex items-center mt-4 underline"><BsTelephone className="mr-2"/>PACH Kaya - 09171862245
+                        </p>
+                    </div>
+                    <div className="w-[50%] p-4">
+                        <form className="w-full h-full flex flex-col justify-between bg-white p-8 rounded-lg shadow-lg">
+                            <div>
+                                <div className="flex justify-between w-full mb-4">
+                                    <input type="text" placeholder="*First Name" className="border border-gray-300 rounded px-4 py-2 w-[48%] focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
+                                    <input type="text" placeholder="*Last Name" className="border border-gray-300 rounded px-4 py-2 w-[48%] focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
+                                </div>
+                                <div className="mb-4">
+                                    <input type="email" placeholder="*Email" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
+                                </div>
+                                <div className="mb-4">
+                                    <input type="tel" placeholder="*Phone Number" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
+                                </div>
+                                <div className="mb-4">
+                                    <input type="text" placeholder="*Subject" className="border border-gray-300 rounded px-4 py-2 w-full focus:outline-none focus:ring-2 focus:ring-[#155C9C]"/>
+                                </div>
+                                <div className="mb-4">
+                                    <textarea placeholder="*Message" className="border border-gray-300 rounded w-full h-[12rem] px-4 py-2 focus:outline-none focus:ring-2 focus:ring-[#155C9C]"></textarea>
+                                </div>
+                            </div>
+                            <button className="py-2 bg-[#155C9C] w-full text-white font-medium rounded hover:bg-[#0D3A5C] transition duration-300">Submit</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
