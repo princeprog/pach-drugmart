2fddb827fdbf8903a3ff26db5040a570dd0921a3
Al Prince Llavan <alprincellavan2019@gmail.com>
2025-01-31 23:44:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/home/', 
+  base: '/home', 
   plugins: [
     react(),
     tailwindcss(),
